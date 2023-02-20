8742d01b9139cd7acf46ca7376601f6135da1d36
tstewart137 <33452705+tstewart137@users.noreply.github.com>
2023-02-20 09:04:46 -0500

    comment out virtual schema/thubnail

--- models/campground.js
+++ models/campground.js
@@ -8,9 +8,9 @@ const ImageSchema = new Schema({
   filename: String
 });
 
-ImageSchema.virtual('thumbnail').get(function () {
+/* ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
-});
+}); */
 
 const ops = {toJSON:{virtuals:true}};
 
